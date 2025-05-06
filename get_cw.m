6a2f5476935987760d5cb8769846eb794c5c9d86
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-05-06 18:12:18 +1000

    create get_cw.m
    преобразование .xls-файлов с данными гидрологии в файлы .hydr

---
+++ get_cw.m
@@ -0,0 +1,40 @@
+close all;
+clear variables;
+clc;
+
+dataset_folder = "FOLDERNAME";
+struct_list = dir(dataset_folder);
+N = size(struct_list);
+
+file_list = cell(N);
+for i = 1:N
+    file_list{i} = [struct_list(i).name];
+end
+
+file_list(1:2) = [];
+N(1) = N(1) - 2;
+
+
+hydr_folder = 'hydrology';
+if not(isfolder(hydr_folder))
+    mkdir(hydr_folder)
+end
+
+dz = 1;
+for i = 1:N
+    file = file_list{i};
+    [~, basename, ~] = fileparts(file);
+
+    table = readtable(fullfile(dataset_folder, file));
+    cw = table.SOUNDVELOCITYM_SEC;
+
+    M = length(cw);
+    depth = (1:dz:M)';
+
+    mat = [depth cw];
+    writematrix(mat, [hydr_folder '\' basename '.hydr'], 'FileType', 'text', 'Delimiter', '\t')
+end
+
+
+
+
