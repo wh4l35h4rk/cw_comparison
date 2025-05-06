003df0c1efa410c630e75a221372895156258652
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-05-06 20:04:45 +1000

    find most different samples

--- find_sample_cws.m
+++ find_sample_cws.m
@@ -6,20 +6,39 @@ foldername = "hydrology";
 file_list = dir(foldername + "\*.hydr");
 file_list = {file_list.name};
 
-N = size(file_list);
-thermocline_files = cell(N);
-same_soundspeed_files = cell(N);
+N = length(file_list);
+thermocline_files = strings(N, 2);
+same_soundspeed_files = strings(N, 2);
 
-for i = 1:N(2)
+for i = 1:N
     file = load(fullfile(foldername, file_list{i}));
-
     c = file(:, 2);
+
     if (max(c) - min(c)) >= 10
-        thermocline_files{i} = file_list{i};
+        thermocline_files(i, 1) = file_list{i};
+        thermocline_files(i, 2) = int2str(max(c) - min(c));
     elseif (max(c) - min(c)) <= 3
-        same_soundspeed_files{i} = file_list{i};
+        same_soundspeed_files(i, 1) = file_list{i};
+        same_soundspeed_files(i, 2) = max(c) - min(c);
     end
 
-    thermocline_files = thermocline_files(~cellfun(@isempty, thermocline_files));
-    same_soundspeed_files = same_soundspeed_files(~cellfun(@isempty, same_soundspeed_files));
+    thermocline_files = thermocline_files(~(thermocline_files(:, 1) == ""), :);
+    same_soundspeed_files = same_soundspeed_files(~(same_soundspeed_files(:, 1) == ""), :);
+
+    thermocline_files = rmmissing(thermocline_files);
+    same_soundspeed_files = rmmissing(same_soundspeed_files);
 end
+
+
+deltas = str2double(thermocline_files(:, 2));
+[~, row] = max(deltas);
+tc_max = thermocline_files(row, :);
+
+deltas = str2double(same_soundspeed_files(:, 2));
+[~, row] = min(deltas);
+ss_min = same_soundspeed_files(row, :);
+
+
+tc_size = length(thermocline_files);
+ss_size = length(same_soundspeed_files);
+
