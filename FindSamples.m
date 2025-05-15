829a6a1651953ee1ae9860fa48901cb51077db97
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-05-16 00:05:49 +1000

    (refactor) convert find_sample_cw to function

---
+++ FindSamples.m
@@ -0,0 +1,59 @@
+function FindSamples(data_folder, samples_folder)
+
+file_list = dir(data_folder + "\*.hydr");
+file_list = {file_list.name};
+
+N = length(file_list);
+thermocline_files = strings(N, 2);
+same_soundspeed_files = strings(N, 2);
+shelf_tc_files = strings(N, 2);
+
+for i = 1:N
+    file = load(fullfile(data_folder, file_list{i}));
+    c = file(:, 2);
+
+    if (max(c) - min(c)) >= 5
+        thermocline_files(i, 1) = file_list{i};
+        thermocline_files(i, 2) = int2str(max(c) - min(c));
+
+        [row_start, row_end] = FindThermocline(file);
+        if (row_end <= 20) && (row_start > 5)
+            shelf_tc_files(i, 1) = file_list{i};
+            shelf_tc_files(i, 2) = thermocline_files(i, 2);
+        end
+
+    elseif (max(c) - min(c)) <= 3
+        same_soundspeed_files(i, 1) = file_list{i};
+        same_soundspeed_files(i, 2) = mean(c);
+    end
+
+    thermocline_files = thermocline_files(~(thermocline_files(:, 1) == ""), :);
+    same_soundspeed_files = same_soundspeed_files(~(same_soundspeed_files(:, 1) == ""), :);
+    shelf_tc_files = shelf_tc_files(~(shelf_tc_files(:, 1) == ""), :);
+
+    thermocline_files = rmmissing(thermocline_files);
+    same_soundspeed_files = rmmissing(same_soundspeed_files);
+    shelf_tc_files = rmmissing(shelf_tc_files);
+end
+
+
+deltas = str2double(shelf_tc_files(:, 2));
+[~, row] = max(deltas);
+tc_max = shelf_tc_files(row, :);
+
+means = str2double(same_soundspeed_files(:, 2));
+[~, row] = min(means);
+ss_min = same_soundspeed_files(row, :);
+
+
+if not(isfolder(samples_folder))
+    mkdir(samples_folder)
+end
+
+copyfile(fullfile(data_folder, tc_max(1)), samples_folder);
+copyfile(fullfile(data_folder, ss_min(1)), samples_folder);
+
+
+PlotResults(data_folder, thermocline_files, same_soundspeed_files, tc_max, ss_min)
+
+end
