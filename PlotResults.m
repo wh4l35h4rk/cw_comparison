d962905b3a6de8d0d2455628efbe49b50d613b81
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-05-26 10:33:05 +1000

    (feat) change plot language

--- PlotResults.m
+++ PlotResults.m
@@ -24,8 +24,8 @@ plot(file(:, 2), file(:, 1), 'Color', 'red', 'LineWidth', 1)
 
 ylim([0, filesize(1)])
 % title("Профили скорости звука: термоклин")
-xlabel("c, м/с")
-ylabel("z, м")
+xlabel("c, m/s")
+ylabel("z, m")
 hold off
 
 
@@ -47,13 +47,15 @@ file = load(fullfile(foldername, ss_min(1)));
 plot(file(:, 2), file(:, 1), 'Color', 'red', 'LineWidth', 1)
 
 % title("Профили скорости звука: циклон")
-xlabel("c, м/с")
-ylabel("z, м")
+xlabel("c, m/s")
+ylabel("z, m")
 hold off
 
 
 
 % ТЕРМОКЛИН И ЦИКЛОН НА ОДНОМ ГРАФИКЕ
+
+p = zeros(ss_size + tc_size + 2);
 
 figure;
 
@@ -63,7 +65,7 @@ for i = 1:tc_size
     c = file(:, 2);
     
     if thermocline_files{i, 1} ~= tc_max(1)
-        plot(c, z, 'Color', [0.8 0.8 0.8], 'LineWidth', 0.3)
+        p(i) = plot(c, z, 'Color', [0.8 0.8 0.8], 'LineWidth', 0.3);
         set(gca, 'YDir', 'reverse')
         hold on
     end
@@ -75,22 +77,24 @@ for i = 1:ss_size
     c = file(:, 2);
     
     if same_soundspeed_files{i, 1} ~= ss_min(1)
-        plot(c, z, 'Color', [0.8 0.8 0.8], 'LineWidth', 0.3)
+        p(i + tc_size) = plot(c, z, 'Color', [0.8 0.8 0.8], 'LineWidth', 0.3);
         set(gca, 'YDir', 'reverse')
         hold on
     end
 end
 
 file = load(fullfile(foldername, ss_min(1)));
-plot(file(:, 2), file(:, 1), 'Color', 'red', 'LineWidth', 1)
+p(end - 1) = plot(file(:, 2), file(:, 1), 'Color', 'blue', 'LineWidth', 1.2, 'DisplayName', "Homogenous cold water");
 
 file = load(fullfile(foldername, tc_max(1)));
 filesize = size(file);
-plot(file(:, 2), file(:, 1), 'Color', 'red', 'LineWidth', 1)
+p(end) = plot(file(:, 2), file(:, 1), 'Color', 'red', 'LineWidth', 1.2, 'DisplayName', "Water with thermocline");
 
 ylim([0, filesize(1)])
-title("Профили скорости звука")
-xlabel("c, м/с")
-ylabel("z, м")
+title("Sound speed profiles")
+xlabel("c, m/s")
+ylabel("z, m")
+legend(p(end - 1:end), Location="eastoutside")
+grid on
 
 hold off
